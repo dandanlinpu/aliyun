4a3dfabe591e6cd8e290d993d256b4d6a5e81230
ldd <273041937@qq.com>
2016-09-25 14:50:21 +0800

    update

---
+++ cTestDemo/string_toc.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+using namespace std;
+int main(){
+   string s("abc");
+   char *cs=s.data();
+
+}

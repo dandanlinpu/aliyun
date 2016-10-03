7dc4f6e94624abbf7f5810d9aab1a7ae936b2358
ldd <273041937@qq.com>
2016-10-03 12:38:41 +0800

    delete some files

--- netProgram/online_matrix_calc_bak/multi_matrix.h
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-vector<vector<int> > multi_matrix(vector<vector<int> > &MatrixA, vector<vector<int> > & MatrixB);
-
--- netProgram/online_matrix_calc_bak/url_encode.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-using namespace std;
-std::string UrlEncode(const std::string& str) ; 
-std::string UrlDecode(const std::string& str) ; 

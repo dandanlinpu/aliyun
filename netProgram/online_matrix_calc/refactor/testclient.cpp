e2032af8bc9dd06909c29db55b11c53edcbb8691
ldd <273041937@qq.com>
2016-10-03 12:35:17 +0800

    delete ditry files

--- netProgram/online_matrix_calc/refactor/testclient.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "socket.h"
-int main(){
-	Tcp_socket s=Tcp_socket();	
- 	int connfd=s.open_clientfd("127.0.0.1",12000);
-}
-
-
--- netProgram/online_matrix_calc/refactor/testserver.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "socket.h"
-int main(){
-	Tcp_socket s=Tcp_socket();	
- 	int listen=s.open_listenfd(12000);
-	int connfd=s.acceptor(s.listenfd);
-}
-
-
